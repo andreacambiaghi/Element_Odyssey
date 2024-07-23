cca065929f35cb425f7ab2e6f34540a0792d820c
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2024-07-23 17:49:33 +0200

    add slider menu

---
+++ Assets/Scripts/SliderMenuAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderMenuAnim : MonoBehaviour
+{
+    public GameObject PanelMenu;
+
+    public void ShowHideMenu()
+    {
+        Debug.Log("Sono denro ShowHideMenu");
+        if(PanelMenu != null)
+        {
+            Debug.Log("PanelMenu non è null");
+            Animator animator = PanelMenu.GetComponent<Animator>();
+            if(animator != null)
+            {
+                Debug.Log("animator non è null");
+                bool isOpen = animator.GetBool("show");
+                animator.SetBool("show", !isOpen);
+            }
+        }
+    }
+}
