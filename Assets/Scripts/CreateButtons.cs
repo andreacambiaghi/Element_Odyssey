a1b822c86b29e04626dd5e3efe91243e52154294
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2024-07-24 16:45:19 +0200

    set anchor

--- Assets/Scripts/CreateButtons.cs
+++ Assets/Scripts/CreateButtons.cs
@@ -19,7 +19,7 @@ public class CreateButtons : MonoBehaviour
         int buttonCount = buttonLabels.Count;
         RectTransform parentRectTransform = GetComponent<RectTransform>();
 
-        float startY = -150f; // Iniziale posizione Y per il primo bottone
+        float startY = 0f; // Iniziale posizione Y per il primo bottone
         float yOffset = -100f; // Offset Y per i successivi bottoni
 
         for (int i = 0; i < buttonLabels.Count; i++)
@@ -41,7 +41,7 @@ public class CreateButtons : MonoBehaviour
 
             // Posiziona il bottone
             RectTransform buttonRectTransform = newButton.GetComponent<RectTransform>();
-            buttonRectTransform.anchoredPosition = new Vector2(0, startY + i * yOffset);
+            //buttonRectTransform.anchoredPosition = new Vector2(0, startY + i * yOffset);
 
             newButton.gameObject.SetActive(true);
         }
