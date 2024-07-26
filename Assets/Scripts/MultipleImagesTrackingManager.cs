30c2b3c1d8a599469b258338a955d5deca65a96f
lorenzo <lorenzo.romeo.mail@gmail.com>
2024-07-26 23:09:17 +0200

    Implemented default object

--- Assets/Scripts/MultipleImagesTrackingManager.cs
+++ Assets/Scripts/MultipleImagesTrackingManager.cs
@@ -9,9 +9,13 @@ using UnityEngine.XR.ARSubsystems;
 public class MultipleImagesTrackingManager : MonoBehaviour
 {
     public static MultipleImagesTrackingManager Instance;
-    [SerializeField] private GameObject[] prefabsToSpawn;
+    private GameObject[] prefabsToSpawn;
     private ARTrackedImageManager _arTrackedImageManager;
+
+    private Dictionary<ARTrackedImage, GameObject> _imageObjectMap; // Marker name -> GameObject
     private Dictionary<string, GameObject> _arObjects;
+
+    public GameObject defaultObject;
 
     // Get the reference to the ARTrackedImageManager
 
@@ -28,6 +32,7 @@ public class MultipleImagesTrackingManager : MonoBehaviour
         }
 
         _arTrackedImageManager = GetComponent<ARTrackedImageManager>();
+        _imageObjectMap = new Dictionary<ARTrackedImage, GameObject>();
         _arObjects = new Dictionary<string, GameObject>();
 
     }
@@ -37,14 +42,11 @@ public class MultipleImagesTrackingManager : MonoBehaviour
         // Listen the event when the tracked images are changed
         _arTrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
 
-        // Spawn the game objects for the existing tracked images
-        foreach (GameObject prefab in prefabsToSpawn)
-        {
-            GameObject newARObject = Instantiate(prefab, Vector3.zero, Quaternion.Euler(-90, 0, 0));
-            newARObject.name = prefab.name;
-            newARObject.gameObject.SetActive(false);
-            _arObjects.Add(newARObject.name, newARObject);
-        }
+        // Spawn the default game object
+        // GameObject newARObject = Instantiate(defaultObject, Vector3.zero, Quaternion.Euler(-90, 0, 0));
+        // newARObject.name = prefab.name;
+        // newARObject.gameObject.SetActive(false);
+        // _arObjects.Add(newARObject.name, newARObject);
     }
 
 
@@ -59,15 +61,27 @@ public class MultipleImagesTrackingManager : MonoBehaviour
         // Identify the changes in the tracked images
         foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
+            // Spawn the prefab when the tracked image is found
+            GameObject newARObject = Instantiate(defaultObject, Vector3.zero, Quaternion.Euler(-90, 0, 0));
+            newARObject.name = defaultObject.name;
+            newARObject.gameObject.SetActive(false);
+            //_arObjects.Add(newARObject.name, newARObject);
+
+            _imageObjectMap.Add(trackedImage, newARObject);
+            Debug.Log("Added: " + trackedImage.referenceImage.name + " -> " + newARObject.name);
+
             UpdatedTrackedImage(trackedImage);
         }
         foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
+            // Update the prefab when the tracked image is updated
             UpdatedTrackedImage(trackedImage);
         }
         foreach (ARTrackedImage trackedImage in eventArgs.removed)
         {
-            _arObjects[trackedImage.referenceImage.name].gameObject.SetActive(false);
+            // Remove the prefab when the tracked image is removed
+            //_arObjects[trackedImage.referenceImage.name].gameObject.SetActive(false);
+            _imageObjectMap[trackedImage].SetActive(false);
         }
     }
 
@@ -76,16 +90,17 @@ public class MultipleImagesTrackingManager : MonoBehaviour
         // Check tracking status of the tracked image
         if (trackedImage.trackingState is TrackingState.Limited or TrackingState.None)
         {
-            _arObjects[trackedImage.referenceImage.name].gameObject.SetActive(false);
+            //_arObjects[trackedImage.referenceImage.name].gameObject.SetActive(false);
+            _imageObjectMap[trackedImage].SetActive(false);
             return;
         }
 
         // Show, hide or position the game object based on the tracking image
-        if (prefabsToSpawn != null)
-        {
-            _arObjects[trackedImage.referenceImage.name].gameObject.SetActive(true);
-            _arObjects[trackedImage.referenceImage.name].transform.position = trackedImage.transform.position;
-        }
+    
+        // _arObjects[trackedImage.referenceImage.name].gameObject.SetActive(true);
+        // _arObjects[trackedImage.referenceImage.name].transform.position = trackedImage.transform.position;
+        _imageObjectMap[trackedImage].gameObject.SetActive(true);
+        _imageObjectMap[trackedImage].transform.position = trackedImage.transform.position;
     }
 
     public void OnPrefabSelected(string prefabName){
