b262b919b9d06a91f885f210ec017bf71e4f5bd2
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2024-07-29 19:22:50 +0200

    add json file for saving achievements

---
+++ Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class AchievementManager : MonoBehaviour
+{
+    private TextAsset jsonFile;
+
+    private string filePath;
+
+    void Awake()
+    {
+        filePath = Path.Combine(Application.dataPath, "Resources", "achievements.json");
+
+        if (!File.Exists(filePath))
+        {
+            Debug.Log("Il file JSON non esiste nel percorso: " + filePath);
+        }
+        else
+        {
+            Debug.Log("Il file JSON esiste nel percorso: " + filePath);
+        }
+        UpdateAchievements();
+    }
+
+    public int GetAchievementValue(string achievementKey)
+    {
+        string json = File.ReadAllText(filePath);
+        Debug.Log(json);
+
+        AchievementWrapper achievementWrapper = JsonUtility.FromJson<AchievementWrapper>(json);
+
+        foreach (var achievement in achievementWrapper.Achievements)
+        {
+            if (achievement.Key == achievementKey)
+            {
+                return achievement.Value;
+            }
+        }
+
+        Debug.Log("Chiave dell'achievement non trovata: " + achievementKey);
+        return -1;
+    }
+
+
+    public void SetAchievementValue(string achievementKey, int value)
+    {
+        string json = File.ReadAllText(filePath);
+        AchievementWrapper achievementWrapper = JsonUtility.FromJson<AchievementWrapper>(json);
+
+        if (achievementWrapper == null)
+        {
+            achievementWrapper = new AchievementWrapper { Achievements = new Achievement[0] };
+        }
+
+        var achievementsList = new List<Achievement>(achievementWrapper.Achievements);
+        var existingAchievement = achievementsList.Find(a => a.Key == achievementKey);
+        
+        if (existingAchievement != null)
+        {
+            existingAchievement.Value = value;
+        }
+        else
+        {
+            achievementsList.Add(new Achievement { Key = achievementKey, Value = value });
+        }
+        achievementWrapper.Achievements = achievementsList.ToArray();
+        string updatedJson = JsonUtility.ToJson(achievementWrapper, true);
+        File.WriteAllText(filePath, updatedJson);
+    }
+
+
+    
+    
+    public void UpdateAchievements()
+    {
+        SetAchievementValue("Achievement 0", 100);
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject achievementObject = FindGameObjectWithUniqueID(i);
+            if (achievementObject != null)
+            {
+                ProgressBar progressBar = achievementObject.GetComponent<ProgressBar>();
+                if (progressBar != null)
+                {
+                    int progress = GetAchievementValue("Achievement " + i);
+                    progressBar.SetProgress(progress);
+                    Debug.Log("progress: " + progress);
+                }
+            }
+        }
+    }
+
+    GameObject FindGameObjectWithUniqueID(int uniqueID)
+    {
+        GameObject[] achievementObjects = GameObject.FindGameObjectsWithTag("Achievement");
+        Debug.Log("achievementObjects.Length: " + achievementObjects.Length);
+        foreach (GameObject obj in achievementObjects)
+        {
+            AchievementIdentifier identifier = obj.GetComponent<AchievementIdentifier>();
+            if (identifier != null && identifier.uniqueID == uniqueID)
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
+}
+
+[System.Serializable]
+public class AchievementWrapper
+{
+    public Achievement[] Achievements;
+}
+
+[System.Serializable]
+public class Achievement
+{
+    public string Key;
+    public int Value;
+}
