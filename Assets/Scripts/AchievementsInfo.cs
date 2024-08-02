7ef5858d07842576fd598f864b9636fb4f312235
lorenzo <lorenzo.romeo.mail@gmail.com>
2024-08-02 17:31:39 +0200

    Fixed compilation problem; Small fixes

--- Assets/Scripts/AchievementsInfo.cs
+++ Assets/Scripts/AchievementsInfo.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AchievementsInfo : MonoBehaviour
+public class AchievementsInfo
 {
     private int[] maxProgress = { 1, 5, 10, 10, 5, 5, 5, 5, 50, 1 };
     private string[] achievemetsInfo = {"Find 1 element", "Find 5 elements", "Find 10 elements", "Play 10 minutes", "Find 5 water elements", "Find 5 fire elements", "Find 5 superheroes elements", "Find 5 elements in five minutes", "Find all elements", "It's a secret" };
+
 
     public int GetMaxProgress(int index)
     {
