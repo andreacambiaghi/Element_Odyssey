970e93b49c3622bc6d4f1f2cd82575f3b0a2cf1d
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2024-09-06 20:21:38 +0200

    modify achievements

--- Assets/Scripts/Achievements/AchievementsInfo.cs
+++ Assets/Scripts/Achievements/AchievementsInfo.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class AchievementsInfo
 {
-    private int[] maxProgress = { 1, 5, 10, 10, 5, 5, 5, 5, 50, 1 };
-    private string[] achievemetsInfo = {"Find 1 element", "Find 5 elements", "Find 10 elements", "Play 10 minutes", "Find 5 water elements", "Find 5 fire elements", "Find 5 superheroes elements", "Find 5 elements in five minutes", "Find all elements", "It's a secret" };
+    private int[] maxProgress = { 1, 5, 10, 10, 5, 5, 5, 5, 5, 50};
+    private string[] achievemetsInfo = {"Find 1 element", "Find 5 elements", "Find 10 elements", "Play 10 minutes", "Find 5 water elements", "Find 5 fire elements","Find 5 earth elements", "Find 5 air elements", "Find 5 elements in 5 minutes", "Find all elements" };
 
 
     public int GetMaxProgress(int index)
