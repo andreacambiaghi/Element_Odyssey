99f405bc759825f1fb93fbb9a4ae921008e60ee8
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2024-11-07 17:15:32 +0100

    fix problem

--- Assets/Scripts/Inventory/Initialize.cs
+++ Assets/Scripts/Inventory/Initialize.cs
@@ -1,48 +1,49 @@
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Initialize : MonoBehaviour
-// {
-//     [SerializeField] private GameObject createButton;
-//     private List<string> labelsToAdd = new List<string>();
-//     ElementFilesManager elementFilesManager;
+public class Initialize : MonoBehaviour
+{
+    [SerializeField] private GameObject createButton;
+    private List<string> labelsToAdd = new List<string>();
+    ElementFilesManager elementFilesManager;
 
-//     void Awake()
-//     {
-//         elementFilesManager = ElementFilesManager.Instance;
+    void Awake()
+    {
+        elementFilesManager = ElementFilesManager.Instance;
 
-//         if(elementFilesManager == null)
-//         {
-//             Debug.LogError("ElementFilesManager non trovato");
-//             return;
-//         }   
+        if(elementFilesManager == null)
+        {
+            Debug.LogError("ElementFilesManager non trovato");
+            return;
+        }   
 
-//         InitializeButtons();
-//     }
+        InitializeButtons();
+    }
  
-//     private void InitializeButtons(){
-//         if(GameModeManager.Instance.GameMode == "createMarker" || GameModeManager.Instance.GameMode == "VirtualPlane"){
-//             labelsToAdd.AddRange(elementFilesManager.GetInitialElements());
-//             labelsToAdd.AddRange(elementFilesManager.GetFoundElements());
-//         }
-//         else if(GameModeManager.Instance.GameMode == "Village") {
-//             ElementFilesManager.VillageData villageData = elementFilesManager.GetVillageData();
-//             foreach(ElementFilesManager.VillageObject villageObject in villageData.villageObjects){
-//                 if(villageObject.Value == 1){
-//                     labelsToAdd.Add(villageObject.Key);
-//                 }
-//             }
-//         }
-//         CreateButtons createButtonsComponent = createButton.GetComponent<CreateButtons>();
-//         if (createButtonsComponent != null && labelsToAdd.Count > 0)
-//         {
-//             createButtonsComponent.buttonLabels.AddRange(labelsToAdd);
-//             Debug.Log("ButtonLabels aggiornato con successo");
-//         }
-//         else
-//         {
-//             Debug.LogError("Il GameObject target non ha il componente 'CreateButtons'");
-//         }
-//     }
+    private void InitializeButtons(){
+        if(GameModeManager.Instance.GameMode == "CreateMarker" || GameModeManager.Instance.GameMode == "VirtualPlane"){
+            labelsToAdd.AddRange(elementFilesManager.GetInitialElements());
+            labelsToAdd.AddRange(elementFilesManager.GetFoundElements());
+        }
+        else if(GameModeManager.Instance.GameMode == "Village") {
+            ElementFilesManager.VillageData villageData = elementFilesManager.GetVillageData();
+            foreach(ElementFilesManager.VillageObject villageObject in villageData.villageObjects){
+                if(villageObject.Value == 1){
+                    labelsToAdd.Add(villageObject.Key);
+                }
+            }
+        }
+        CreateButtons createButtonsComponent = createButton.GetComponent<CreateButtons>();
+        Debug.LogError(labelsToAdd.Count);
+        if (createButtonsComponent != null && labelsToAdd.Count > 0)
+        {
+            createButtonsComponent.buttonLabels.AddRange(labelsToAdd);
+            Debug.Log("ButtonLabels aggiornato con successo");
+        }
+        else
+        {
+            Debug.LogError("Il GameObject target non ha il componente 'CreateButtons'");
+        }
+    }
 
-// }
+}
