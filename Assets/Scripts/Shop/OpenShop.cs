1ea389d13d02ecf1193a461551ad74dd6304f26e
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2025-02-10 14:03:34 +0100

    add open/close shop menu

--- Assets/Scripts/Shop/OpenShop.cs
+++ Assets/Scripts/Shop/OpenShop.cs
@@ -1,22 +1,32 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenShop : MonoBehaviour
 {
     [Header("GameObjects da Disabilitare")]
     [SerializeField] private GameObject slider;
+    [SerializeField] private SliderMenuAnim menu;
     [SerializeField] private GameObject volume;
     [SerializeField] private GameObject home;
+    [SerializeField] private GameObject elementSelected;
+    [SerializeField] private GameObject openShop;
 
     [Header("GameObject da Attivare")]
     [SerializeField] private GameObject shop;
     [SerializeField] private GameObject shopClose;
 
-    public void Click()
+    public void Open()
     {
-        Debug.LogError("Shop aperto");
+        Debug.Log("Shop aperto");
+        
         if (slider != null)
         {
+            if (menu.GetState())
+            {
+                Button button = slider.GetComponent<Button>();
+                button.onClick.Invoke();
+            }
             slider.SetActive(false);
         }
 
@@ -30,6 +40,16 @@ public class OpenShop : MonoBehaviour
             home.SetActive(false);
         }
 
+        if (elementSelected != null)
+        {
+            elementSelected.SetActive(false);
+        }
+
+        if (openShop != null)
+        {
+            openShop.SetActive(false);
+        }
+
         if (shop != null)
         {
             shop.SetActive(true);
@@ -38,6 +58,44 @@ public class OpenShop : MonoBehaviour
         if (shopClose != null)
         {
             shopClose.SetActive(true);
+        }
+    }
+    public void Close()
+    {
+        Debug.Log("Shop chiuso");
+        if (slider != null)
+        {
+            slider.SetActive(true);
+        }
+
+        if (volume != null)
+        {
+            volume.SetActive(true);
+        }
+
+        if (home != null)
+        {
+            home.SetActive(true);
+        }
+
+        if (elementSelected != null)
+        {
+            elementSelected.SetActive(true);
+        }
+
+        if (openShop != null)
+        {
+            openShop.SetActive(true);
+        }
+
+        if (shop != null)
+        {
+            shop.SetActive(false);
+        }
+
+        if (shopClose != null)
+        {
+            shopClose.SetActive(false);
         }
     }
 }
