195358552a316264f54792be3a32613faae824b8
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2025-02-13 11:48:44 +0100

    fix click floor in inventory

--- Assets/Scripts/InventoryFloor/ClickFloor.cs
+++ Assets/Scripts/InventoryFloor/ClickFloor.cs
@@ -3,18 +3,20 @@ using UnityEngine.UI;
 
 public class ClickFloor : MonoBehaviour
 {
-    private Material outlineMaterial;
-    private bool isSelected = false;
+    private Button floorButton;
 
     void Start()
     {
-        outlineMaterial = GetComponent<Image>().material;
-        outlineMaterial.SetFloat("_OutlineThickness", 0f); // Contorno inizialmente spento
+        floorButton = GetComponent<Button>();
+
+        if (floorButton != null)
+        {
+            floorButton.onClick.AddListener(OnButtonClick);
+        }
     }
 
-    public void ToggleOutline()
+    void OnButtonClick()
     {
-        isSelected = !isSelected;
-        outlineMaterial.SetFloat("_OutlineThickness", isSelected ? 0.02f : 0f);
+        Debug.Log("Bottone cliccato!");
     }
 }
