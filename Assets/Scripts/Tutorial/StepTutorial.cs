b091ffa93a8c180cd5f5ff33c402f2f3d60f3a16
Andrea Cambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2025-04-19 13:03:34 +0200

    finish tutorail

--- Assets/Scripts/Tutorial/StepTutorial.cs
+++ Assets/Scripts/Tutorial/StepTutorial.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class StepTutorial : MonoBehaviour
 {
     [SerializeField] private GameObject clickOpenMenu;
+    [SerializeField] private GameObject clickOpenMenu2;
     [SerializeField] private GameObject waterFrame;
     [SerializeField] private GameObject fireFrame;
     [SerializeField] private GameObject achievementButton;
@@ -12,12 +13,17 @@ public class StepTutorial : MonoBehaviour
     [SerializeField] private GameObject homeButton;
     [SerializeField] private GameObject slider;
     [SerializeField] private GameObject unionElement;
-    [SerializeField] private GameObject firstElement;
+    [SerializeField] private GameObject showAchievement;
     [SerializeField] private GameObject openInventory;
     [SerializeField] private GameObject clickAchievementButton;
     [SerializeField] private GameObject clickCardBoardButton;
-    [SerializeField] private GameObject achievementExplanation;
+    [SerializeField] private GameObject combineElement;
     [SerializeField] private GameObject cardBoardExplanation;
+    [SerializeField] private GameObject nextStepButton;
+    [SerializeField] private GameObject clickCloseAchievementButton;
+    [SerializeField] private GameObject clickCloseCardBoardButton;
+    [SerializeField] private GameObject forChange;
+    [SerializeField] private GameObject finalTutorial;
 
     void Awake()
     {
@@ -26,6 +32,8 @@ public class StepTutorial : MonoBehaviour
         cardBoardButton.SetActive(false);
         homeButton.SetActive(false);
         slider.SetActive(false);
+        nextStepButton.SetActive(false);
+        clickOpenMenu2.SetActive(false);
     }
 
     private void Step1()
@@ -42,27 +50,79 @@ public class StepTutorial : MonoBehaviour
     private void Step3()
     {
         Destroy(GameObject.Find(fireFrame.name + "(Clone)"));
-        GameObject first = Instantiate(firstElement, firstElement.transform.position, Quaternion.identity);
-        StartCoroutine(ExecuteAfterTime(3f, () => 
-        {
-            Destroy(first);
-            GameObject inventory = Instantiate(openInventory, openInventory.transform.position, Quaternion.identity);
-            StartCoroutine(ExecuteAfterTime(3f, () => 
-            {
-                Destroy(inventory);
-                clickOpenMenu.SetActive(true);
-                slider.SetActive(true);
-                achievementButton.SetActive(true);
-            }));
-        }));
+        Instantiate(combineElement);
     }
 
-    private IEnumerator ExecuteAfterTime(float time, System.Action action)
+    private void Step4()
     {
-        yield return new WaitForSeconds(time);
-        action?.Invoke();
+        Destroy(GameObject.Find(combineElement.name + "(Clone)"));
+        Instantiate(unionElement);
+        nextStepButton.SetActive(true);
     }
 
+    private void Step5()
+    {
+        Destroy(GameObject.Find(unionElement.name + "(Clone)"));
+        Instantiate(openInventory);
+        clickOpenMenu.SetActive(true);
+        slider.SetActive(true);
+        nextStepButton.SetActive(false);
+    }
+
+    private void Step6()
+    {
+        Destroy(GameObject.Find(openInventory.name + "(Clone)"));
+        clickOpenMenu.SetActive(false);
+        clickOpenMenu2.SetActive(true);
+    }
+
+    private void Step7()
+    {
+        clickOpenMenu2.SetActive(false);
+        Instantiate(showAchievement);
+        clickAchievementButton.SetActive(true);
+        achievementButton.SetActive(true);
+    }
+
+    private void Step8()
+    {
+        Destroy(GameObject.Find(showAchievement.name + "(Clone)"));
+        clickAchievementButton.SetActive(false);
+    }
+
+    private void Step9()
+    {
+        clickCloseAchievementButton.SetActive(false);
+        Instantiate(cardBoardExplanation);
+        cardBoardButton.SetActive(true);
+    }
+
+    private void Step10()
+    {
+        Destroy(GameObject.Find(cardBoardExplanation.name + "(Clone)"));
+        clickCardBoardButton.SetActive(false);
+        clickCloseCardBoardButton.SetActive(true);
+    }
+
+    private void Step11()
+    {
+        clickCloseCardBoardButton.SetActive(false);
+        Instantiate(forChange);
+        nextStepButton.SetActive(true);
+    }
+
+    private void Step12()
+    {
+        Destroy(GameObject.Find(forChange.name + "(Clone)"));
+        Instantiate(finalTutorial);
+    }
+
+    private void Step13()
+    {
+        Destroy(GameObject.Find(finalTutorial.name + "(Clone)"));
+        homeButton.SetActive(true);
+        nextStepButton.SetActive(false);
+    }
     private int currentStep = 0;
 
     public void NextStep()
@@ -79,6 +139,36 @@ public class StepTutorial : MonoBehaviour
                 break;
             case 3:
                 Step3();
+                break;
+            case 4:
+                Step4();
+                break;
+            case 5:
+                Step5();
+                break;
+            case 6:
+                Step6();
+                break;
+            case 7:
+                Step7();
+                break;
+            case 8:
+                Step8();
+                break;
+            case 9:
+                Step9();
+                break;
+            case 10:
+                Step10();
+                break;
+            case 11:
+                Step11();
+                break;
+            case 12:
+                Step12();
+                break;
+            case 13:
+                Step13();
                 break;
             // Aggiungi altri case per step aggiuntivi
         }
